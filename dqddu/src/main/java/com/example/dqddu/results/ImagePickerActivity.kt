a627fda5cec60306b876252da6bc689540a1327e
DengQi <dengqi02@maoyan.com>
2021-05-17 16:24:09 +0800

    DQ - Activity Results API - 建立[ImagePicker]跳转页面

---
+++ dqddu/src/main/java/com/example/dqddu/results/ImagePickerActivity.kt
@@ -0,0 +1,10 @@
+package com.example.dqddu.results
+
+import com.example.dqddu.base.BaseBindingActivity
+import com.example.dqddu.databinding.ActivityImagePickerBinding
+
+class ImagePickerActivity : BaseBindingActivity<ActivityImagePickerBinding>() {
+
+    override fun initBinding() = ActivityImagePickerBinding.inflate(layoutInflater)
+
+}
